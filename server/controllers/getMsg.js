f6a254fa30494e75c051a76e777f0077c00a0405
y122972 <y122972@gmail.com>
2019-03-27 22:42:02 +0800

    新增标签显示，按标签搜索

--- server/controllers/getMsg.js
+++ server/controllers/getMsg.js
@@ -7,15 +7,21 @@ module.exports=router
 router.get('/getArticleList', (req, res) => {
     console.log('getList: ' + req.query.page + ',' + req.query.pageSize,req.query.msg,req.query.option)
     if(req.query.msg&&req.query.option){
-        let titleLike=`%${req.query.msg}%`,contentLike=`%${req.query.msg}%`
+        let titleLike=`%${req.query.msg}%`,contentLike=`%${req.query.msg}%`,labelLike=`%${req.query.msg}%`
         if(req.query.option=='title'){
             contentLike=null
+            labelLike=null
             console.log('title',titleLike,contentLike)
         } else if(req.query.option=='content'){
             titleLike=null
+            labelLike=null
             console.log('content',titleLike,contentLike)
+        } else if(req.query.option=='label'){
+            titleLike=null
+            contentLike=null
+            console.log('label',titleLike,contentLike)
         }
-        db.query('select id,title,front,time,label from article where deleted=0 and (title like ? or content like ?) order by time DESC limit ?,?;select count(*) as total from article where deleted=0 and (title like ? or content like ?)', [titleLike,contentLike,req.query.page * req.query.pageSize - 0, req.query.pageSize - 0,titleLike,contentLike], rows => {
+        db.query('select id,title,front,time,label from article where deleted=0 and (title like ? or content like ? or label like ?) order by time DESC limit ?,?;select count(*) as total from article where deleted=0 and (title like ? or content like ? or label like ?)', [titleLike,contentLike,labelLike,req.query.page * req.query.pageSize - 0, req.query.pageSize - 0,titleLike,contentLike,labelLike], rows => {
             res.send(rows)
         })
     } else {
